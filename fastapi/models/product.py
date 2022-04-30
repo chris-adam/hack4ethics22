590c89d1b7be2bbee71651ec5e7b3b4718138734
Chris Adam <chris.adam@b12-consulting.com>
2022-04-30 19:18:41 +0200

    Cleaned deployment

--- fastapi/models/product.py
+++ fastapi/models/product.py
@@ -18,26 +18,12 @@ def get_all_products(country_name: str):
     AND pr.ProductName CONTAINS 'Bases'
     RETURN pr.ProductName
     """
-    return graph.run(query).to_data_frame().to_string()
+    return graph.run(query).to_data_frame().loc[:, "Product"].to_string()
 
 
 class Product():
 
     def __init__(self, product_name: str, decision: datetime.date, expiration_date: datetime.date):
-        port      = "7687"
-        user      = "neo4j"
-        passw     = "07xLgl5sy7uUazqtWh1QvuRHSTIkkUZ_kMz5Hqg8Twc"
-        db_name   = "neo4j"
-        uri       = "neo4j+s://2abbd1fc.databases.neo4j.io"
-
-        graph = Graph(uri, auth=(user, passw))
-        query = """
-        MATCH (w:Winery)-[:FROM_PROVENCE]->(p:Province)-[:PROVINCE_COUNTRY]->(c:Country)
-        RETURN c.name AS Country, count(DISTINCT w) AS totalNrWineries
-        ORDER BY totalNrWineries DESC LIMIT 10
-        """
-        graph.run(query).to_data_frame()
-
         self._product_name = product_name
         self._decision = decision
         self._expiration_date = expiration_date
