0f717cf64619ec08251273fc58ea7542095f3806
Chris Adam <chris.adam@b12-consulting.com>
2022-04-30 14:57:44 +0200

    Set up deployment files

--- fastapi/main.py
+++ fastapi/main.py
@@ -6,3 +6,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == '__main__':
+    uvicorn.run("main:app",  host="0.0.0.0", port=80, reload=True, debug=False)
